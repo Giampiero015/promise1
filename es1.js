1f8fba1255d79a753846166190ef4be7a787e76e
Giampiero015 <150815405+Giampiero015@users.noreply.github.com>
2023-12-04 16:20:51 +0100

    Update es1.js

--- es1.js
+++ es1.js
@@ -6,16 +6,16 @@ const ogg =
 const ogg2=null;
 function fetchDataFromAPI(data) {
     console.log("recupero dati in corso... ");
-    setTimeOut(()=>{
+    
             return new Promise((resolve,reject) => {
-            if(data){
-                resolve(data);
-            }else{
-                reject('ko');
-            }
+            setTimeOut(()=>{
+                if(data){
+                    resolve(data);
+                }else{
+                    reject('ko');
+                }
+            },1000);   
         });
-    },1000);
-   
   }
 fetchDataFromAPI(ogg)   .then((data)=>{console.log(data)})
                         .catch((error)=>{console.log(error)});
