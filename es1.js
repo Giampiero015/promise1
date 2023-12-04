49baccaad45058b07f4dd6a704388854f2075733
Giampiero015 <150815405+Giampiero015@users.noreply.github.com>
2023-12-04 14:54:46 +0100

    Add files via upload

--- es1.js
+++ es1.js
@@ -1,1 +1,20 @@
-//prova
+const ogg = 
+{ 
+    name: "John",
+    age: 30 
+};
+const ogg2=null;
+function fetchDataFromAPI(data) {
+    console.log("recupero dati in corso... ");
+    return new Promise((resolve,reject) => {
+        if(data){
+            resolve(data);
+        }else{
+            reject('ko');
+        }
+    });
+  }
+fetchDataFromAPI(ogg)   .then((data)=>{console.log(data)})
+                        .catch((error)=>{console.log(error)});
+fetchDataFromAPI(ogg2)   .then((data)=>{console.log(data)})
+                        .catch((error)=>{console.log(error)});
